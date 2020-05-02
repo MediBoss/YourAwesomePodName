d7c62e68644bde270540c685fee5ce681e42e45a
Medi Assumani <mediassumani49@gmail.com>
2020-05-01 22:31:23 -0700

    initial commmit

---
+++ PK/yourAwesomePodName/Source.swift
@@ -0,0 +1,26 @@
+//
+//  Source.swift
+//  yourAwesomePodName
+//
+//  Created by Medi Assumani on 5/1/20.
+//  Copyright © 2020 Medi Assumani. All rights reserved.
+//
+
+import Foundation
+
+/**
+ 
+ 
+        Your implementation of the pod here
+ */
+
+class SomeFancyClassName {
+    
+    func getAllUsers() {
+        
+    }
+    
+    func removeAllUsers() {
+        
+    }
+}
---
+++ PK/yourAwesomePodName/yourAwesomePodName.h
@@ -0,0 +1,19 @@
+//
+//  yourAwesomePodName.h
+//  yourAwesomePodName
+//
+//  Created by Medi Assumani on 5/1/20.
+//  Copyright © 2020 Medi Assumani. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for yourAwesomePodName.
+FOUNDATION_EXPORT double yourAwesomePodNameVersionNumber;
+
+//! Project version string for yourAwesomePodName.
+FOUNDATION_EXPORT const unsigned char yourAwesomePodNameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <yourAwesomePodName/PublicHeader.h>
+
+
